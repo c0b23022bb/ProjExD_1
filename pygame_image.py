cc7cf4ef934425ce11935770b06cfa68dfd229f4
c0b23022bb <c0b23022bb@edu.teu.ac.jp>
2024-06-11 16:52:00 +0900

    練習8まで完成

--- pygame_image.py
+++ pygame_image.py
@@ -10,17 +10,34 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img1 = pg.image.load("fig/3.png")
-    bg_img1 = pg.transform.flip(bg_img1,True,False)
+    koukaton = pg.image.load("fig/3.png")
+    koukaton = pg.transform.flip(koukaton,True,False)
+    bg_img2 = pg.transform.flip(bg_img,True,False)
+    kkrect = koukaton.get_rect()
+    kkrect.center = 300,200
     tmr = 0
+    img_x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
-        screen.blit(bg_img1, [300, 200])
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x + 1600,0])
+        screen.blit(bg_img, [-x + 3200, 0])
+        screen.blit(bg_img2, [-x + 4800, 0])
+        screen.blit(koukaton, kkrect.center)
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kkrect.move_ip((0,-1))
+        elif key_lst[pg.K_DOWN]:
+            kkrect.move_ip((0,1))
+        elif key_lst[pg.K_LEFT]:
+            kkrect.move_ip((-1,0))
+        elif key_lst[pg.K_RIGHT]:
+            kkrect.move_ip((1,0))
         pg.display.update()
-        tmr += 1        
+        tmr += 1
+        img_x = img_x - 1        
         clock.tick(200)
 
 
